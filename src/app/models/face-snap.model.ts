07bc4ecd08c5770a163f59e8e9c70fab1d134737
marine <marinou.boulanger@gmail.com>
2023-10-02 23:51:58 +0200

    lesson about ngIf

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,21 +1,32 @@
 export class FaceSnap {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  location?: string;
+
+  //we can  also do:
   //   title: string;
   //   description: string;
   //   createdDate: Date;
   //   snaps: number;
   //   imageUrl: string;
 
-  constructor(
-    public title: string,
-    public description: string,
-    public createdDate: Date,
-    public snaps: number,
-    public imageUrl: string
-  ) {
-    // this.title = title;
-    // this.description = description;
-    // this.createdDate = createdDate;
-    // this.snaps = snaps;
-    // this.imageUrl = imageUrl;
-  }
+  // this.title = title;
+  // this.description = description;
+  // this.createdDate = createdDate;
+  // this.snaps = snaps;
+  // this.imageUrl = imageUrl;
+
+  //or we can do:
+  // constructor(
+  //   public title: string,
+  //   public description: string,
+  //   public createdDate: Date,
+  //   public snaps: number,
+  //   public imageUrl: string,
+  //   public location?: string
+  // ) {
+  // }
 }
