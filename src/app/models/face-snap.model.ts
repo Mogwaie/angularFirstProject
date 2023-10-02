a0f50318e21b3f3df8e1e886a9092b71b55468bd
marine <marinou.boulanger@gmail.com>
2023-10-02 17:22:49 +0200

    add personalised property

---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,21 @@
+export class FaceSnap {
+  //   title: string;
+  //   description: string;
+  //   createdDate: Date;
+  //   snaps: number;
+  //   imageUrl: string;
+
+  constructor(
+    public title: string,
+    public description: string,
+    public createdDate: Date,
+    public snaps: number,
+    public imageUrl: string
+  ) {
+    // this.title = title;
+    // this.description = description;
+    // this.createdDate = createdDate;
+    // this.snaps = snaps;
+    // this.imageUrl = imageUrl;
+  }
+}
